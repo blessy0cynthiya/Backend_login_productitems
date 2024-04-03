6ab8b64c2c41589b2941002e17aca067d2bf76dd
blessy0cynthiya <125266702+blessy0cynthiya@users.noreply.github.com>
2024-04-03 10:52:51 +0530

    Add files via upload

---
+++ demo1/demo1/src/main/java/com/example/demo1/service/ProductService.java
@@ -0,0 +1,56 @@
+/*package com.example.demo1.service;
+
+public class ProductService {
+    
+} */
+package com.example.demo1.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo1.model.Product;
+import com.example.demo1.repository.ProductRepo;
+@Service
+public class ProductService {
+    @Autowired
+    ProductRepo mr;
+    public Product create (Product mm)
+    {
+        return mr.save(mm);
+    }
+    public List<Product> getAll()
+    {
+        return mr.findAll();
+    }
+    public Product getMe(int id)
+    {
+        return mr.findById(id).orElse(null);
+    }
+    public boolean updateDetails(int id,Product mm)
+        {
+            if(this.getMe(id)==null)
+            {
+                return false;
+            }
+            try{
+                mr.save(mm);
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            return true;
+        }
+public boolean deleteProduct(int id)
+        {
+            if(this.getMe(id) == null)
+            {
+                return false;
+            }
+            mr.deleteById(id);
+            return true;
+        }
+
+}
